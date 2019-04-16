49d5a5b292d2f32d435a03ad2b14c530f500c042
Jens Trio <jens@trio.sexy>
2019-04-16 10:11:50 +0200

    :tada: Initial commit

---
+++ lintflix.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+messages=(
+    "Linto the wild"
+    "Schindler's Lint"
+    "Brace yourselves, Linter is coming"
+    "Linterstellar"
+    "The Lintouchables"
+    "La La Lint"
+    "James Bond - Lint Another Day"
+    "Mission Impossible - Lint protocol"
+    "Lintiana Jones and the Last Crusade"
+    "La Linta e Bella"
+    "The Hobbit: An Unexpected Lint"
+    "Sudden Lintpact"
+    "The Termilinter"
+    "Good Will Linting"
+    "Lint Free or Die Hard"
+    "The Lintruder"
+    "Lost In Translinting"
+    "The Flintstones"
+    "Men in Black: Linternational"
+    "Lint me like one of your French girls"
+    "To Lintfinity – and beyond!"
+    "Lintglourious Basterds"
+    "Lintception"
+    "Lintin' in the Rain"
+    "Silver Lintings Playbook"
+)
+
+git commit -m ":rotating_light: ${messages[$RANDOM % ${#messages[@]} ]} :cinema:"
